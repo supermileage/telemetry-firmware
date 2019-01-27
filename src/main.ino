1f1c6ca8607df0661369e8b4957feca437045c9f
lcheng1 <3242609+lcheng1@users.noreply.github.com>
2019-01-26 18:44:03 -0800

    Added library files

--- src/main.ino
+++ src/main.ino
@@ -2,9 +2,7 @@
 #include "can-telemetry.h"
 /**
  * Preliminary support for arbitrary CAN support on the Particle IO, 
- * for the Supermileage telemetry project. 
- * 
- *
+ * for the Supermileage telemetry project.
  */
 const bool DEBUG          = true;
 const int BAUD_RATE       = 50000;
@@ -14,37 +12,13 @@ const int TIMEOUT_T   = 1000;
 const int NODE_ID     = 1;
 const int CMD_ID      = 0x009; // The request cmd ID on the ODrive
 
-// SYSTEM_THREAD(ENABLED); // Non-blocking threading for Particle.publish()
-
-/**
- * Creates a new CANChannel object to connect to a CAN bus on 
- * specified pins. It takes 3 parameters.
- * CANChannel can(pins, rxQueueSize, txQueueSize)
- * @param pins CAN_D1_D2 for the Electron
- * @param rxQueueSize the queue size for received messages, default 32
- * @param txQueueSize the queue size for transmitted messages, default 32
- */
-CANChannel can(CAN_D1_D2); // set a bigger queue just in case. TODO test this
-CANTelemetry tele(can, 50000, true);
-
+CANChannel can(CAN_D1_D2);
+CANTelemetry tele(can, BAUD_RATE, TIMEOUT_T, DEBUG);
 // Time 
 unsigned long time_ms;
 
 // Our message ID. It should probably be a constant
 uint32_t command = (NODE_ID << 5) + CMD_ID;
-
-/**
- * For reference, the CANMessage object (sent and received by 
- * our CANChannel) is as follows
-struct CANMessage
-{
-   uint32_t id;
-   bool     extended;
-   bool     rtr;
-   uint8_t  len;
-   uint8_t  data[8];
-}
-*/
 
 void setup() {
   Serial.begin(9600);
