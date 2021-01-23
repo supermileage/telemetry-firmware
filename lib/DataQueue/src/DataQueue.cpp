be853f6612ffa7da148d39e09c5246b815255634
DFriend01 <69879126+DFriend01@users.noreply.github.com>
2021-01-23 12:07:57 -0800

    Async queue class. (#11)
    * Initial commit with DataQueue.
    
    * Updated cpp file
    
    * Async class is working. Made changes to JsonMaker.
    
    * Cleaned up duplicated code and added more documentation for the JsonMaker class.

---
+++ lib/DataQueue/src/DataQueue.cpp
@@ -0,0 +1,29 @@
+#include "DataQueue.h"
+
+DataQueue::DataQueue() {
+	init();
+}
+
+void DataQueue::add(String id, int value) {
+	jsonMaker->add(id, value);
+}
+
+void DataQueue::add(String id, String value) {
+	jsonMaker->add(id, value);
+}
+
+void DataQueue::add(String id, float value) {
+	jsonMaker->add(id, value);
+}
+
+void DataQueue::publish(String event, PublishFlags flag1, PublishFlags flag2) {
+	String payload = jsonMaker->get();
+	publishQueue->publish(event, payload, flag1, flag2);
+	jsonMaker->refresh();
+}
+
+void DataQueue::init() {
+	publishQueue = new PublishQueueAsyncRetained(publishQueueRetainedBuffer, uint16_t(sizeof(publishQueueRetainedBuffer)));
+	jsonMaker = new JsonMaker();
+	publishQueue->setup();
+}
--- lib/JsonMaker/src/JsonMaker.cpp
+++ lib/JsonMaker/src/JsonMaker.cpp
@@ -1,24 +1,42 @@
 #include "JsonMaker.h"
 
-/**
- *  Constructor 
- **/
-JsonMaker::JsonMaker(){
-    // Clear Buffer and JSONBufferWriter object
-    memset(buf, 0, sizeof(buf));
-    this->writer = new JSONBufferWriter(buf, sizeof(buf) - 1);
-
-    Time.zone(-8);
+JsonMaker::JsonMaker() {
     init();
 }
 
-/**
- * Deletes the JSONBufferWriter object if it is not null and creates
- * a new writer object. The buffer is cleared. Init JSON string.
- **/
-void JsonMaker::init(){
-    // Destroy previous JSONBufferWriter object, clear mem, and create new one
+void JsonMaker::add(String id, int value) {
+    writer->beginObject();
+    writer->name("t").value(id);
+    writer->name("d").value(value);
+    writer->endObject();
+}
+
+void JsonMaker::add(String id, String value) {
+    writer->beginObject();
+    writer->name("t").value(id);
+    writer->name("d").value(value);
+    writer->endObject();
+}
+
+void JsonMaker::add(String id, float value) {
+    writer->beginObject();
+    writer->name("t").value(id);
+    writer->name("d").value(value);
+    writer->endObject();
+}
+
+String JsonMaker::get() {
+    writer->endArray();
+    writer->endObject();
+    return String(buf);
+}
+
+void JsonMaker::refresh() {
     if(this->writer != NULL) delete this->writer;
+    init();
+}
+
+void JsonMaker::init() {
     memset(buf, 0, sizeof(buf));
     this->writer = new JSONBufferWriter(buf, sizeof(buf) - 1);
 
@@ -26,54 +44,3 @@ void JsonMaker::init(){
     writer->name("time").value((int)Time.now());
     writer->name("d").beginArray();
 }
-
-/**
- * Add a new object to the buffer with an int value
- * 
- * @param id: the id for the value
- * @param value: the integer value corresponding to its id
- **/
-void JsonMaker::add(String id, int value){
-    writer->beginObject();
-    writer->name("t").value(id);
-    writer->name("d").value(value);
-    writer->endObject();
-}
-
-/**
- * Adds a new object to the buffer with a string value.
- * 
- * @param id: the id for the value
- * @param value: the string value corresponding to its id
- **/
-void JsonMaker::add(String id, String value){
-    writer->beginObject();
-    writer->name("t").value(id);
-    writer->name("d").value(value);
-    writer->endObject();
-}
-
-/**
- * Adds a new object to the buffer with a string value.
- * 
- * @param id: the id for the value
- * @param value: the float value corresponding to its id
- **/
-void JsonMaker::add(String id, float value){
-    writer->beginObject();
-    writer->name("t").value(id);
-    writer->name("d").value(value);
-    writer->endObject();
-}
-
-/**
- * Ends the JSON object and returns its string representation
- * 
- * @return a JSON string
- **/
-String JsonMaker::get(){
-    writer->endArray();
-    writer->endObject();
-    return String(buf);
-}
-
