69eb7817adbb6fcc85fa61870899d52c4405608a
Silviu Toderita <silviu.toderita@gmail.com>
2021-04-04 09:40:29 -0700

    fixed urban code and added SparkFun_GNSS as submodule

--- urban/src/main.cpp
+++ urban/src/main.cpp
@@ -1,7 +1,6 @@
 #include "Particle.h"
 #include "Arduino.h"
 #include "JsonMaker.h"
-#include "Sensor_ECU.h"
 
 #define BLINK_INTERVAL_OFF 1800
 #define BLINK_INTERVAL_ON 200
@@ -9,30 +8,13 @@
 
 SYSTEM_THREAD(ENABLED);
 
-void onSerialData();
 JsonMaker json_maker;
-Sensor_ECU ecu(&Serial1);
 
-
-uint32_t last_blink = 0; // Time of last blink
 uint32_t last_publish = 0; // Time of last publish
-boolean led_state = LOW;
 
-Timer timer(1, onSerialData);
 
-// Handler for any new message on ID "Proto"
-void proto_response(const char *event, const char *data) {
-    Serial.println("Received Message on ID: " + String(event) + " - Message Data: " + String(data));
-}
 
 void setup() {
-    pinMode(D7, OUTPUT);
-
-    ecu.begin();
-    timer.start();
-
-    // Subscribe to any new messages on ID "Proto"
-    Particle.subscribe("hook-response/Proto", proto_response, MY_DEVICES);
 
     // Setup function only runs after Boron connected in (default) Automatic mode
     Serial.println("Particle Connected!");
@@ -41,30 +23,11 @@ void setup() {
 
 void loop() {
 
-    digitalWrite(D7, led_state);
-    
-    // Blink the LED
-    if (led_state & (millis() - last_blink >= BLINK_INTERVAL_ON)){
-        led_state = LOW;
-        last_blink = millis();
-    }else if(!led_state & (millis() - last_blink >= BLINK_INTERVAL_OFF)){
-        led_state = HIGH;
-        last_blink = millis();
-    }
-
-    //Publish a message to Proto
+    //Publish a message
     if (millis() - last_publish >= PUBLISH_INTERVAL){
         last_publish = millis();
-        //Call makeJSON function
-        json_maker.add("PROTO-RPM", ecu.getRPM());
-        json_maker.add("PROTO-SPARK", ecu.getSpark());
-        Particle.publish("Proto", json_maker.get(), PRIVATE, WITH_ACK);
     }
 
 }
 
-void onSerialData()
-{
-    ecu.handle();
-}
 
