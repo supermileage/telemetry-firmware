65b322f89efaa69a0a067a66ffb1ccd62ac4a140
Silviu Toderita <silviu.toderita@gmail.com>
2021-04-13 20:55:24 -0700

    updated makefile

--- proto/src/build.mk
+++ proto/src/build.mk
@@ -1,2 +1,11 @@
-INCLUDE_DIRS += $(SOURCE_PATH)/lib/Adafruit-MAX31855/
-INCLUDE_DIRS += $(SOURCE_PATH)/lib/Adafruit-BusIO/
+# Standard behavior must be included here
+INCLUDE_DIRS += $(SOURCE_PATH)/$(USRSRC) # add user sources to include path
+CPPSRC += $(call target_files,$(USRSRC_SLASH),*.cpp)
+CSRC += $(call target_files,$(USRSRC_SLASH),*.c)
+ 
+APPSOURCES=$(call target_files,$(USRSRC_SLASH),*.cpp)
+APPSOURCES+=$(call target_files,$(USRSRC_SLASH),*.c)
+ 
+# Custom stuff can be added here
+INCLUDE_DIRS += $(APPDIR)/lib/Adafruit-MAX31855/
+INCLUDE_DIRS += $(APPDIR)/lib/Adafruit-BusIO/
