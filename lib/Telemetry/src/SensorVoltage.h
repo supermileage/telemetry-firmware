8955e57f9d0000fa2ea636f0174e036496759648
Valentino Jaber <91361373+valentino-jaber@users.noreply.github.com>
2021-11-08 21:23:27 -0800

    Update SensorVoltage.h

--- lib/Telemetry/src/SensorVoltage.h
+++ lib/Telemetry/src/SensorVoltage.h
@@ -4,6 +4,11 @@
 #include "Particle.h"
 #include "Sensor.h"
 
+#define INPUT_VOLTAGE_PIN A1
+#define ANALOG_CONVERT_RAW_TO_3V 1230
+#define R1 10000
+#define R2 2200
+
 class SensorVoltage : public Sensor
 {
     public:
