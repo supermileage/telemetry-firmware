85d6f3be5a6f3aee9755268e66507f3f744a4b3e
Silviu Toderita <silviu.toderita@gmail.com>
2021-12-16 18:50:22 -0800

    changes brackets

--- src/System/CanInterface.cpp
+++ src/System/CanInterface.cpp
@@ -20,8 +20,9 @@ void CanInterface::handle() {
             unsigned long canId = _CAN->getCanId();
 
             // check if we're listening for canId, continue if we're not
-            if (_messages.find(canId) == _messages.end())
+            if (_messages.find(canId) == _messages.end()) {
                 continue;
+            }
 
             CanMessage& message = _messages[canId];
             message.dataLength = len;
