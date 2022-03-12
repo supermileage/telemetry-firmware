7a890d2a14c27579f3aab1d9d35da4cc3674f605
Silviu Toderita <silviu.toderita@gmail.com>
2022-03-11 21:13:46 -0800

    updated BMS fault codes

--- src/Sensor/BmsFault.h
+++ src/Sensor/BmsFault.h
@@ -6,29 +6,31 @@ namespace BmsFault {
         NONE=0,
         VOLTAGE_LOW,
         VOLTAGE_HIGH, 
-        VOLTAGE_VERY_HIGH,
+        CELL_OVER_5V,
         TEMP_LOW,
         TEMP_HIGH, 
         TEMP_BMS_HIGH,
+        THERMISTOR_PACK,
+        THERMISTOR_INTERNAL, 
         CURRENT_DISCHARGE_HIGH,
         CURRENT_CHARGE_HIGH,
         CURRENT_REGEN_HIGH,
-        SWITCH_CHARGE,
-        SWITCH_DISCHARGE,
         CURRENT_SENSOR_DISCONNECT,
         CURRENT_SENSOR_CONNECT,
+        SWITCH_CHARGE,
+        SWITCH_DISCHARGE,
         SOFTWARE,
         HARDWARE,
-        CELL_ASIC,
+        INTERNAL_LOGIC,
+        COMMUNICATION_INTERNAL,
+        COMMUNICATION_CAN,
         OPEN_WIRING,
         WEAK_CELL,
         WEAK_PACK,
         BALANCING,
-        COMM_INTERNAL,
-        COMM_EXTERNAL,
         POWER_SUPPLY,
         HIGH_VOLTAGE_ISOLATION,
-        THERMISTOR    
+        CELL_BANK    
     };
 
     inline String toString(uint8_t code) {
@@ -39,34 +41,42 @@ namespace BmsFault {
                 return "Low Voltage";
             case VOLTAGE_HIGH:
                 return "High Voltage";
-            case VOLTAGE_VERY_HIGH:
-                return "Very High Voltage";
+            case CELL_OVER_5V:
+                return "Cell Voltage >5v";
             case TEMP_LOW:
                 return "Low Temperature";
             case TEMP_HIGH:
                 return "High Temperature";
             case TEMP_BMS_HIGH:
                 return "High BMS Temperature";
+            case THERMISTOR_PACK:
+                return "Battery Pack Thermistor";
+            case THERMISTOR_INTERNAL,
+                return "BMS THermistor"
             case CURRENT_DISCHARGE_HIGH:
                 return "High Discharge Current";
             case CURRENT_CHARGE_HIGH:
                 return "High Charge Current";
             case CURRENT_REGEN_HIGH:
                 return "High Regen Current";
-            case SWITCH_CHARGE:
-                return "Charge Switch";
-            case SWITCH_DISCHARGE:
-                return "Discharge Switch";
             case CURRENT_SENSOR_DISCONNECT:
                 return "Current Sensor Disconnected";
             case CURRENT_SENSOR_CONNECT:
                 return "Current Sensor Connected";
+            case SWITCH_CHARGE:
+                return "Charge Switch";
+            case SWITCH_DISCHARGE:
+                return "Discharge Switch";
             case SOFTWARE:
                 return "Software";
             case HARDWARE:
                 return "Hardware";
-            case CELL_ASIC:
-                return "Cell ASIC";
+            case INTERNAL_LOGIC:
+                return "Internal Logic";
+            case COMMUNICATION_INTERNAL:
+                return "Internal Communication";
+            case COMMUNICATION_CAN:
+                return "CAN Communication";
             case OPEN_WIRING:
                 return "Open Wiring";
             case WEAK_CELL:
@@ -75,16 +85,12 @@ namespace BmsFault {
                 return "Weak Pack";
             case BALANCING:
                 return "Balancing";
-            case COMM_INTERNAL:
-                return "Internal Communication";
-            case COMM_EXTERNAL:
-                return "External Communication";
             case POWER_SUPPLY:
                 return "Power Supply";
             case HIGH_VOLTAGE_ISOLATION:
                 return "High Voltage Isolation";
-            case THERMISTOR:
-                return "Thermistor";
+            case CELL_BANK:
+                return "Cell Bank";
             default:
                 return "Unknown";
 
