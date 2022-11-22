49b172cff7ade51a22ff9b1d895a5cd5bca53fcf
Valentino Jaber <valentino.jaber@gmail.com>
2022-11-21 22:09:51 -0800

    WIP: Test Case Improvements

--- test/tests/Sensor/CanSensorTinyBmsTest.cpp
+++ test/tests/Sensor/CanSensorTinyBmsTest.cpp
@@ -7,16 +7,20 @@
 #include "CanSensorTinyBms.h"
 
 /* Helper Functions */
+void packBatteryData(float voltage, float current, float soc, uint8_t* buf);
+void packCellData(float cellVoltageLow, float cellVoltageHigh, float cellVoltageAvg, uint8_t* buf);
+void packTempData(int8_t packTempLow, int8_t packTempHigh, int8_t packTempAvg, int8_t bmsTemp, uint8_t* buf);
+void packInt16(int16_t val, uint8_t* buf);
 
 template <typename T>
-void testValidation(CanSensorTinyBms& bms, T (CanSensorTinyBms::*getter)(bool&)) {
+void testValidation(CanSensorTinyBms& tiny, T (CanSensorTinyBms::*getter)(bool&)) {
 	bool statusIsValid = false;
 	setMillis(DEFAULT_STALE_TIME_MILLIS - 1);
-	(bms.*getter)(statusIsValid);
+	(tiny.*getter)(statusIsValid);
 	REQUIRE ( statusIsValid );
 
 	setMillis(DEFAULT_STALE_TIME_MILLIS);
-	(bms.*getter)(statusIsValid);
+	(tiny.*getter)(statusIsValid);
 	REQUIRE_FALSE ( statusIsValid );
 }
 
@@ -26,15 +30,15 @@ const int orderedFaults[] = {};
 /* Tests */
 TEST_CASE( "CanSensorTinyBms::getHumanName", "[CanSensorTinyBms][Sensor]" ) {
 	CanInterface interface(nullptr);
-	CanSensorTinyBms bms(interface);
+	CanSensorTinyBms tiny(interface, 500);
 
-	REQUIRE( bms.getHumanName().equals("CanSensorTinyBms") );
+	REQUIRE( tiny.getHumanName().equals("CanSensorTinyBms") );
 }
 
 TEST_CASE( "CanSensorTinyBms::update CAN_TINYBMS_REQUEST", "[CanSensorTinyBms][Sensor][CanSensor]" ) {
 	CanBusMock canBusMock(CAN_MESSAGE_AVAIL_TEST);
 	CanInterface interface(&canBusMock);
-	CanSensorTinyBms bms(interface);
+	CanSensorTinyBms tiny(interface, 500);
 
 	setMillis(DEFAULT_START_TIME_MILLIS);
 
@@ -46,31 +50,63 @@ TEST_CASE( "CanSensorTinyBms::update CAN_TINYBMS_REQUEST", "[CanSensorTinyBms][S
 
 	SECTION( "YO" ) {
 
-		testValidation(bms, &CanSensorTinyBms::getSOMETHING);
+		//testValidation(tiny, &CanSensorTinyBms::get);
 	}
-
-	REQUIRE( mock.messageAvail() == 1 );
 }
 
-TEST_CASE( "CanSensorTinyBms::update CAN_TINYBMS_RESPONSE", "[CanSensorTinyBms][Sensor][CanSensor]" ) {
+TEST_CASE( "CanSensorTinyBms::update CAN_TINYBMS_TEMP", "[CanSensorTinyBms][Sensor][CanSensor]" ) {
 	CanBusMock canBusMock(CAN_MESSAGE_AVAIL_TEST);
 	CanInterface interface(&canBusMock);
-	CanSensorTinyBms bms(interface);
+	CanSensorTinyBms tiny(interface, 500);		//TEMP TESTS
 
 	setMillis(DEFAULT_START_TIME_MILLIS);
 
 	CanMessage msg = CAN_MESSAGE_NULL;
-	msg.id = CAN_TINYBMS_RESPONSE;
-	msg.dataLength = TINYBMS_REQ_DATA_LENGTH;
-
-	Handler::instance().begin();
+	msg.id = TINYBMS_PARAM_ID_TEMP;
+	msg.dataLength = TINYBMS_REQ_DATA_LENGTH - 2;
 
 	SECTION( "YO" ) {
 
-		testValidation(bms, &CanSensorTinyBms::getSOMETHING);
-	}
+		// set up pack message with battery voltage value
+		packTempData(0, 0, 24, 0, test);
+		canBusMock.setCanMessage(msg);
 
-	REQUIRE( mock.messageAvail() == 1 );
+		// act
+		Handler::instance().handle();
+
+		// assert
+		bool minTempIsValid = false;
+		REQUIRE ( tiny.getTempBms(bmsTempIsValid) == 24);
+		REQUIRE ( bmsTempIsValid );
+		REQUIRE ( orion.getMaxBatteryTemp() == 0 );
+		REQUIRE ( orion.getAvgBatteryTemp() == 0 );
+		REQUIRE ( orion.getMinBatteryTemp() == 0 );
+
+		testValidation(tiny, &CanSensorTinyBms::getTempBms);
+	}
 }
 
 /* Helper Function Definitions */
+void packBatteryData(float voltage, float current, float soc, uint8_t* buf) {
+	packInt16((int16_t)(voltage * 10.0f), buf);
+	packInt16((int16_t)(current * 10.0f), buf + 2);
+	buf[4] = (uint8_t)(soc * 2.0f);
+}
+
+void packCellData(float cellVoltageLow, float cellVoltageHigh, float cellVoltageAvg, uint8_t* buf) {
+	packInt16((int16_t)(cellVoltageLow * 1000), buf);
+	packInt16((int16_t)(cellVoltageHigh * 1000), buf + 2);
+	packInt16((int16_t)(cellVoltageAvg * 1000), buf + 4);
+}
+
+void packTempData(int8_t packTempLow, int8_t packTempHigh, int8_t packTempAvg, int8_t bmsTemp, uint8_t* buf) {
+	buf[0] = packTempLow;
+	buf[1] = packTempHigh;
+	buf[2] = packTempAvg;
+	buf[3] = bmsTemp;
+}
+
+void packInt16(int16_t val, uint8_t* buf) {
+	*buf = val >> 8;
+	*(buf + 1) = val & 0xFF;
+}
