dcd53bd8f1fbcc3ebac8e8053120f830f905d5e9
cosparks <colby.r.sparks@gmail.com>
2023-05-17 09:23:56 -0700

    Adds gps init status to serial debug output

--- src/vehicleFc.cpp
+++ src/vehicleFc.cpp
@@ -91,6 +91,7 @@ void CurrentVehicle::debugSensorData() {
     DEBUG_SERIAL("Input Voltage: "+ String(inVoltage.getVoltage()) + "v - ");
     DEBUG_SERIAL_LN("Internal Temp: " + String(thermo1.getInternalTemp()) + "°C");
     // GPS
+	DEBUG_SERIAL("GPS Init: " + gps.getInitStatus() + " - ");
     DEBUG_SERIAL("Longitude: " + gps.getLongitude() + "° - ");
     DEBUG_SERIAL("Latitude: " + gps.getLatitude() + "° - ");
     DEBUG_SERIAL("Heading: " + String(gps.getHeading()) + "° - ");
